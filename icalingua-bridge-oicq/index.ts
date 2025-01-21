0af8dcb38baf943f784ee223b96afee0e3296de2
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2025-01-21 23:35:39 +0800

    style: prettier

--- icalingua-bridge-oicq/index.ts
+++ icalingua-bridge-oicq/index.ts
@@ -3,9 +3,9 @@ import { config, userConfig } from './providers/configManager'
 import { init as initSocketIo } from './providers/socketIoProvider'
 import onebotAdapter from './adapters/onebotAdapter'
 
-process.on('unhandledRejection', error => {
-    console.error('UnhandledException: ', error);
-});
+process.on('unhandledRejection', (error) => {
+    console.error('UnhandledException: ', error)
+})
 
 let adapter: typeof oicqAdapter
 
